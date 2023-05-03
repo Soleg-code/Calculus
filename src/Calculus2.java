7ab7e28cecf920dcc6d816622c376dd00a1443f0
Antoine CHAING <antoine.chaing@epita.fr>
2023-05-03 14:08:04 +0200

    fix: functionality with multiple function working

--- src/Calculus2.java
+++ src/Calculus2.java
@@ -9,6 +9,9 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.CubicCurve2D;
+import java.util.List;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,12 +22,18 @@ public class Calculus2 extends JFrame {
     private JPanel mainPanel;
     private JPanel questionPanel;
     private JPanel answerPanel;
+    private JButton nextButton;
+    private int indexQuestion = 0;
+    private List<Question> questions;
+    private int score;
+    private int total;
 
-    public Calculus2() {
-        super("QCM");
+    public Calculus2(List<Question> questions) {
+        super("CALCULUS");
 
+        this.questions = questions;
         mainPanel = new JPanel(new GridBagLayout());
-        mainPanel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(200, 200, 200, 200));
 
         questionPanel = new JPanel();
         questionPanel.setBackground(Color.WHITE);
@@ -39,15 +48,31 @@ public class Calculus2 extends JFrame {
                 BorderFactory.createLineBorder(Color.GRAY, 1),
                 BorderFactory.createEmptyBorder(20, 20, 20, 20)));
 
+        
+
+
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
         c.insets = new Insets(0, 0, 10, 0);
         c.anchor = GridBagConstraints.CENTER;
         mainPanel.add(questionPanel, c);
-
         c.gridy = 1;
         mainPanel.add(answerPanel, c);
+
+        nextButton = new JButton("Suivant");
+        nextButton.setPreferredSize(new Dimension(100, 30));
+        nextButton.setFont(new Font("Arial", Font.PLAIN, 14));
+        nextButton.setEnabled(false);
+        nextButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                nextQuestion();
+            }
+        });
+
+
+        c.gridy = 2;
+        mainPanel.add(nextButton,c);
 
         this.getContentPane().add(mainPanel);
 
@@ -55,6 +80,55 @@ public class Calculus2 extends JFrame {
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.pack();
+
+        score = 0;
+        total = questions.size();
+
+        showCurrentQuestion();
+    }
+
+    private void showCurrentQuestion()
+    {
+        Question question = questions.get(indexQuestion);
+        setQuestion(question.getQuestion());
+        for (String answer : question.getAnswers()) {
+            if (answer.equals(question.getAnswers().get(question.getCorrectAnswer()))) {
+                addAnswerButton(answer, true);
+            } else {
+                addAnswerButton(answer, false);
+            }
+        }
+    }
+
+    private void clearAnswerPanel()
+    {
+        answerPanel.removeAll();
+        answerPanel.revalidate();
+        answerPanel.repaint();
+    }
+
+    private void nextQuestion()
+    {
+        indexQuestion++;
+        if (indexQuestion < questions.size()) {
+            questionPanel.removeAll();
+            answerPanel.removeAll();
+            // nextButton.setEnabled(false);
+            showCurrentQuestion();
+            nextButton.setVisible(false);
+        } else {
+            // this.dispose();
+            double percent = (double) score / total * 100;
+            JLabel scoreLabel = new JLabel(String.format("Score: %.2f%%", percent));
+            scoreLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+            questionPanel.removeAll();
+            questionPanel.add(scoreLabel);
+            answerPanel.removeAll();
+            nextButton.setText("Terminer");
+            nextButton.setVisible(true);
+            nextButton.setEnabled(true);
+            // need to redirect to the menu.
+        }
     }
 
     public void setQuestion(String questionText) {
@@ -69,28 +143,33 @@ public class Calculus2 extends JFrame {
         answerButton.setFont(new Font("Arial", Font.PLAIN, 20));
         answerButton.setFocusPainted(false);
 
-        if (isCorrect) {
-            //answerButton.setBackground(Color.decode("#B7EB8F"));
-            answerButton.setBackground(new Color(255,255,255));
-        } else {
-            answerButton.setBackground(new Color(255,255,255));
-            //answerButton.setBackground(Color.decode("#EBA18F"));
-        }
-
         answerButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if (isCorrect) {
-                    answerButton.setBackground(new Color(0, 128, 0)); // Change color to dark green if correct
-                } else {
-                    answerButton.setBackground(Color.RED); // Keep red color if incorrect
-                }
-                for (Component component : answerPanel.getComponents()) {
-                    if (component instanceof JButton) {
-                        component.setEnabled(false); // Disable all answer buttons after selection
-                    }
-                }
+                checkAnswer(answerButton, isCorrect);
             }
         });
+
+        // if (isCorrect) {
+        //     answerButton.setBackground(new Color(255,255,255));
+        // } else {
+        //     answerButton.setBackground(new Color(255,255,255));
+        // }
+
+        // answerButton.addActionListener(new ActionListener() {
+        //     public void actionPerformed(ActionEvent e) {
+        //         if (isCorrect) {
+        //             answerButton.setBackground(new Color(0, 128, 0)); // Change color to dark green if correct
+        //         } else {
+        //             answerButton.setBackground(Color.RED); // Keep red color if incorrect
+        //         }
+        //         for (Component component : answerPanel.getComponents()) {
+        //             if (component instanceof JButton) {
+        //                 component.setEnabled(false); // Disable all answer buttons after selection
+        //                 nextButton.setEnabled(true);
+        //             }
+        //         }
+        //     }
+        // });
 
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
@@ -98,23 +177,29 @@ public class Calculus2 extends JFrame {
         c.insets = new Insets(5, 0, 5, 0);
         c.anchor = GridBagConstraints.CENTER;
         answerPanel.add(answerButton, c);
+        answerButton.revalidate();
+        answerButton.repaint();
+    }
+
+    private void checkAnswer(JButton answerButton, boolean isCorrect) {
+        answerButton.setEnabled(false);
+        if (isCorrect) {
+            answerButton.setBackground(Color.GREEN); // Change color to dark green if correct
+            score++;
+            nextButton.setEnabled(true);
+        } else {
+            answerButton.setBackground(Color.RED); // Keep red color if incorrect
+            nextButton.setEnabled(true);
+        }
+        nextButton.setVisible(true);
     }
 
     public static void main(String[] args) {
-        Calculus2 qcmInterface = new Calculus2();
-        qcmInterface.setQuestion("Quelle est la capitale de la France ?");
-        qcmInterface.addAnswerButton("Paris", true);
-        qcmInterface.addAnswerButton("Berlin", false);
-        qcmInterface.addAnswerButton("Londres", false);
-        qcmInterface.addAnswerButton("Rome", false);
-        qcmInterface.addAnswerButton("Madrid", false);
-
-        // qcmInterface.removeAll();
-        // qcmInterface.setQuestion("Quelle est la capitale de l'Allemagne ?");
-        // qcmInterface.addAnswerButton("Paris", false);
-        // qcmInterface.addAnswerButton("Berlin", true);
-        // qcmInterface.addAnswerButton("Londres", false);
-        // qcmInterface.addAnswerButton("Rome", false);
-        // qcmInterface.addAnswerButton("Madrid", false);
+        List<String> answers = List.of("Paris", "Berlin", "Londres", "Rome", "Madrid");
+        Question question = new Question("Quelle est la capitale de la France ?", answers, 0);
+        Question question2 = new Question("Quelle est la capitale de l'Allemagne ?", answers, 1);
+        Question question3 = new Question("Quelle est la capitale de l'Angleterre ?", answers, 2);
+        List<Question> questions = List.of(question, question2, question3);
+        Calculus2 qcmInterface = new Calculus2(questions);
     }
 }
